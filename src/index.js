711c7b68fac54dc5712cbf3b6a3a6d08eab554f2
Антон <antonvolynskii@gmail.com>
2020-05-27 15:06:27 +1000

    created data for small movie cards, then created templates using this data and inserted those templates in DOM, then added mouseEnter listener for each movie

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app';
-
+import App from './components/app/app.jsx';
+import {smallCards} from './mocks/films'
+const init = (gameQuestions) => {
+  const settings = {
+    gameGenre: '10110101010'
+  }
 
 ReactDOM.render(
-<App />,
+<App
+gameGenre={settings.gameGenre}
+smallCardsData={gameQuestions}
+/>,
   document.querySelector('#root'),
 );
+}
+init(smallCards)
+console.log(smallCards)
---
+++ src/mocks/films.js
@@ -0,0 +1,29 @@
+const smallCards = [
+  {
+  src:'img/fantastic-beasts-the-crimes-of-grindelwald.jpg',
+  alt:'Fantastic Beasts: The Crimes of Grindelwald',
+  name:'Fantastic Beasts: The Crimes of Grindelwald'
+},
+    {
+      src:'img/bohemian-rhapsody.jpg',
+      alt:'Bohemian Rhapsody',
+      name:'Bohemian Rhapsody'
+    },
+  {
+    src:'img/macbeth.jpg',
+    alt:'Macbeth',
+    name:'Macbeth'
+  },
+  {
+    src:'img/aviator.jpg',
+    alt:'Aviator',
+    name:'Aviator'
+  },
+  {
+    src:'img/we-need-to-talk-about-kevin.jpg',
+    alt:'We need to talk about Kevin',
+    name:'We need to talk about Kevin'
+  }
+]
+
+export {smallCards}
